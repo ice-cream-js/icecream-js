52e4fcdc486ff3bf96bd3d5b8688340232bffffb
MyferIsADev <contactme.myfer@protonmail.com>
2024-05-28 17:57:40 -0500

    ✨Make app.js

---
+++ src/example/app.js
@@ -0,0 +1,12 @@
+import render from "./icecream.js";
+
+render('app',
+`<h1>hello world</h1>
+`,`
+:root {
+    color:white;
+    text-align:center;
+    background-color:black;
+    font-family:Arial, sans serif;
+}
+`)
