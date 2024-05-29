fec3ab98a212c20e48a5ea77c3d42eadff250b6c
MyferIsADev <contactme.myfer@protonmail.com>
2024-05-28 23:25:35 -0500

    Improve example `./example/app.js` file.

--- example/app.js
+++ example/app.js
@@ -1,12 +1,21 @@
 import render from "./lib/icecream.js";
 
-render('app',
-`<h1>hello world</h1>
+render('app',`
+</p>
+<p align="center">
+<img src="https://img.shields.io/badge/npm-false-red"> <img src="https://img.shields.io/badge/cli-coming_soon-blue"> <img src="https://img.shields.io/badge/developer-myfer-green"> <br><img src="https://img.shields.io/badge/builds-passing-brightgreen"> <img src="https://img.shields.io/badge/release-v1.0.0-brightgreen">
+<br><br>
+<a href="https://github.com/ice-cream-js/icecream-js/wiki#Documetation">Documentation.</a>
+</p>
 `,`
 :root {
     color:white;
     text-align:center;
-    background-color:black;
+    background-color:#202125;
     font-family:Arial, sans serif;
+}
+
+a {
+    color:#fca7df;
 }
 `)
